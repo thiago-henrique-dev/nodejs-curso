3c7baa91d3e946bcb0f4a76efee165c241a185ad
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-11-28 00:11:53 -0200

    Curso de Node.js - Parâmetros #08

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get("/sobre", function(req, res){
 app.get("/blog", function(req, res){
     res.send("Bem vindo ao meu blog!")
 })
+
+app.get("/ola/:cargo/:nome/:cor", function(req, res){
+    res.send("<h1>Ola "+req.params.nome+"</h1>"+"<h2> Seu cargo é: "+req.params.cargo+"</h2>"+"<h3> Sua cor favorita é: "+req.params.cor+"</h3>");
+    
+    
+})
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081")
 
