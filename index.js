b9a299aaa8302913bf8e63b67bdab73bff94540c
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-11-28 09:37:45 -0200

    Curso de Node.js - Nodemon #09

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.end("Seja bem vindo ao meu app!")
+    res.end("Seja bem vindo ao meu app!!")
 })
 app.get("/sobre", function(req, res){
     res.send("Minha pagina sobre")
@@ -20,3 +20,4 @@ app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081")
 
 })
+//Instalando nodemon
